07772dd868cd424a8c331628494813bfd7b44f97
SachinGameDev <contact.ssingh@gmail.com>
2020-09-20 19:59:47 +0530

    Scene Management with 3 Scenes Initial commit

---
+++ Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems; 
+
+public class SceneLoader : MonoBehaviour
+{
+    public void OnLoadSceneButtonPressed()
+    {
+        var button = EventSystem.current.currentSelectedGameObject;
+
+        int buildIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (button.name == "BackButton")
+        {
+            buildIndex--;
+        }
+        else
+        {
+            buildIndex++;
+
+        }
+
+        SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
+
+    }
+
+
+
+}
